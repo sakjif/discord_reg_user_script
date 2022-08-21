bc876d44af4ae4df4d0105cda762bedc3ae80f04
Medet <medik121@gmail.com>
2022-08-21 19:10:31 +0600

    put print messege out of loop

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ password = ''.join(random.choice(string.ascii_letters) for i in range(15))
 
 def register(email, date_of_birth, username, password):
     request_captchaID = requests.get(f'http://2captcha.com/in.php?key={api_key}&method=hcaptcha&sitekey={site_key}&pageurl={discord_url}')
-    captchaID = request_captchaID.content[3:].decode("utf-8")
+    captchaID = request_captchaID.text
+    captchaID = captchaID.replace("OK|", "")
+    print("Идет обработка hCAPTCHA...")
     while True:
         request_captcha_token = requests.get(f'http://2captcha.com/res.php?key={api_key}&action=get&id={captchaID}')
-        print("Идет обработка hCAPTCHA")
-        if "OK" in request_captcha_token:
-            captcha_token = request_captcha_token.content[3:].decode("utf-8")
+        captcha_token = request_captcha_token.text
+        if "OK" in captcha_token:
+            captcha_token = captcha_token.replace("OK|", "")
             payload = {
                 "date_of_birth": date_of_birth,
                 "email": email,
